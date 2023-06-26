010451d4c9c71c1ab9fc4820d51b09e46773adab
Tahasoomro12 <136990084+Tahasoomro12@users.noreply.github.com>
2023-06-26 08:25:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,231 @@
+// var sindh =[ "Islamabad", "Ahmed Nager", "Ahmadpur East", "Ali Khan", "Alipur"];
+// var punj  = ["Alipur", "Arifwala", "Attock", "Bhera", "Bhalwal", "Bahawalnagar"];
+// var kpe =["Bahawalpur", "Bhakkar", "Burewala", "Chillianwala", "Chakwal", "Chichawatni"];
+// var cars =["Chiniot", "Chishtian", "Daska", "Darya Khan", "Dera Ghazi", "Dhaular", "Dina"];
+// var red_1 = ["Dinga", "Dipalpur", "Faisalabad", "Fateh Jhang", "Ghakhar Mandi", "Gojra", "Gujranwala"];
+// var red_2 = ["Gujrat", "Gujar Khan", "Hafizabad", "Haroonabad", "Hasilpur", "Haveli", "Lakha", "Jalalpur"];
+// var red_3 = ["Jattan", "Jampur", "Jaranwala", "Jhang", "Jhelum", "Kalabagh", "Karor Lal", "Kasur", "Kamalia"]; 
+// var /red_4 = ["Kamoke", "Khanewal","Khanpur", "Kharian", "Kistan"];
+
+// var quizQuestions = [
+//     {
+//       num: 1,
+//       question: "HTML stand for",
+//       Option: {
+//         a: "Hyper text markup Language",
+//         b: "Hyper text programming Language",
+//         c: "Hyper text styling Language",
+//         d: "Hyper text scripting Language",
+//       },
+//       answer: "Hyper text markup Language",
+//     },
+//     {
+//       num: 2,
+//       question: "Which type of JavaScript Languages is",
+//       Option: {
+//         a: "Object-Oriented ",
+//         b: "Object-Base",
+//         c: "Assembly Languages",
+//         d: "high Level",
+//       },
+//       answer: "Object-Base",
+//     },
+//     {
+//       num: 3,
+//       question: "The 'function' and  'var' are known as:",
+//       Option: {
+//         a: "Keywords",
+//         b: "Data types",
+//         c: "Declaration statements",
+//         d: "Prototypes",
+//       },
+//       answer: "Declaration statements",
+//     },
+//     {
+//       num: 4,
+//       question: "who is the present president of pakistan",
+//       Option: {
+//         a: "Arif Alvi",
+//         b: "Imran Khan",
+//         c: "Nawaz Sharif",
+//         d: "Zardari",
+//       },
+//       answer: "Arif Alvi",
+//     },
+//     {
+//       num: 5,
+//       question: "How many prayers in a day:",
+//       Option: {
+//         a: "6",
+//         b: "5",
+//         c: "3",
+//         d: "none",
+//       },
+//       answer: "5",
+//     },
+//     {
+//       num: 6,
+//       question: "How many total surah in quran",
+//       Option: {
+//         a: "113",
+//         b: "114",
+//         c: "112",
+//         d: "111",
+//       },
+//       answer: "114",
+//     },
+//     {
+//       num: 7,
+//       question: "The correct sequence of HTML tags for starting a webpage is",
+//       Option: {
+//         a: "Head, Title, HTML, body",
+//         b: "HTML, Body, Title, Head",
+//         c: "HTML, Head, Title, Body",
+//         d: "HTML, Title , Head,  Body",
+//       },
+//       answer: "HTML, Head, Title, Body",
+//     },
+//   ];
+
+
+// var head = document.getElementById("heading");
+// var total = document.getElementById("total");
+// var pt = document.getElementsByClassName("option")
+
+// var counter = 0;
+
+// function nxt(){
+//     heading.innerHTML = quizQuestions[counter].question;
+//     pt[0].innerHTML = quizQuestions[counter].Option.a
+//     pt[1].innerHTML = quizQuestions[counter].Option.b 
+//     pt[2].innerHTML = quizQuestions[counter].Option.c
+//     pt[3].innerHTML = quizQuestions[counter].Option.d
+
+// }
+
+// function gone(){
+//     counter++
+
+//     if(counter < quizQuestions.length){
+//        nxt()
+//        total.innerHTML =+ counter + 1 + "/" + quizQuestions.length;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+var counter = 0;
+
+function total(){
+counter++
+var don = document.getElementById("last");
+don.innerHTML = counter++ 
+}
+
+
+
+
+
+function von(){
+    counter--
+    var easy = document.getElementById("hok");
+    easy.innerHTML = counter--
+    }
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
